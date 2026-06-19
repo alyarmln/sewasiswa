0defa6d6e0db51f109d8226006dac7fbb9f43071
Skadi Dragnor <85822403+Blackaiza@users.noreply.github.com>
2026-06-19 18:15:13 +0800

    Create database.php for database connection
    Added database connection script with configuration variables so no need to called in each files

---
+++ database.php
@@ -0,0 +1,12 @@
+<?php 
+
+#Database Connection , no need to implement in all files
+# Means panggil je file ni dalam semua file yang guna DB
+
+$VPS_HOST = {{secrets.VPS_HOST}}
+$VPS_USERNAME_DB = {{secrets.VPS_USERNAME_DB}}
+$VPS_PASS_DB = {{secrets.VPS_PASS_DB}}
+$VPS_NAME_DB = {{secrets.VPS_NAME_DB}}
+
+
+$conn = mysqli_connect( $VPS_HOST, $VPS_USERNAME_DB , $VPS_PASS_DB , $VPS_NAME_DB );
