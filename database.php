98b36dbdf051f4f27b46489821888dd7abcae30e
alyarmln <nralyarmln@gmail.com>
2026-06-21 18:34:20 +0800

    update ver 1

--- database.php
+++ database.php
@@ -6,7 +6,7 @@
 #ini test untuk version control serta guna GIT PUSH , PULL
 
 
-#Aizat akan gantikan file ini untuk dijadikan .env
+#Aizat akan gantikan file ini untuk dijadikan .env temp
 
 $VPS_HOST = {{secrets.VPS_HOST}}
 $VPS_USERNAME_DB = {{secrets.VPS_USERNAME_DB}}
