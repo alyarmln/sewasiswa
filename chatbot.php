754d7e0795f16794b1fa85e04cb631bab1433d45
Aizat <aizathamizan2@gmail.com>
2026-06-30 11:00:50 +0800

    Update this deploy 23 - Fix Code SECURE GERMINI

--- chatbot.php
+++ chatbot.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SewaSiswa - AI Assistant</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <script src="https://cdn.tailwindcss.com"></script><meta name="google-signin-client_id" content="85903086524-sdd23qq381911tdf7jv0baarhlk244om.apps.googleusercontent.com.apps.googleusercontent.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
